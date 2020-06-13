6a1ec52cc502791e82245734577a37bc1ee95c7d
gutovesco <guto.vesco@hotmail.com>
2020-06-13 15:27:43 -0300

    bot

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
-// Supports ES6
-// import { create, Whatsapp } from 'sulla';
-const sulla = require('sulla');
+const wa = require('@open-wa/wa-automate');
 
-sulla.create().then((client) => start(client));
+wa.create().then(client => start(client));
 
 function start(client) {
-  client.onMessage((message) => {
-    if(message.body){
-      client.sendText(message.from, 'Olá');
+  client.onMessage(message => {
+    console.log(message)
+    if (message.body === 'Oi' || message.body === 'oi') {
+
+      while(message.body !== '4'){
+      client.sendText(message.from, 'Olá! O que você gostaria de fazer ? \n1 - Pausar\n2- Próxima \n4- Encerrar');
+
+        switch(message.body){
+          case '1':
+            client.sendText(message.from, 'Pausando a musica...');
+            break;
+  
+          case '2':
+            client.sendText(message.from, 'Pulando para a próxima musica');
+            break;
+        }
+      }
+      
     }
   });
 }
