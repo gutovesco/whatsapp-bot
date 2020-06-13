f13368cde7542f56cf3eded1b2c673893f691a08
gutovesco <guto.vesco@hotmail.com>
2020-06-13 16:24:37 -0300

    bot

--- index.js
+++ index.js
@@ -2,25 +2,27 @@ const wa = require('@open-wa/wa-automate');
 
 wa.create().then(client => start(client));
 
+var acidente = false
+
 function start(client) {
   client.onMessage(message => {
-    console.log(message)
-    if (message.body === 'Oi' || message.body === 'oi') {
 
-      while(message.body !== '4'){
-      client.sendText(message.from, 'Olá! O que você gostaria de fazer ? \n1 - Pausar\n2- Próxima \n4- Encerrar');
+    switch(message.body){
+      case '1':
+        client.sendText(message.from, 'teste');
+        break;
 
-        switch(message.body){
-          case '1':
-            client.sendText(message.from, 'Pausando a musica...');
-            break;
-  
-          case '2':
-            client.sendText(message.from, 'Pulando para a próxima musica');
-            break;
+      case '2':
+        client.sendText(message.from, 'Você deseja reportar o acidente por áudio ou chat?');
+        acidente = true
+
+      default:
+        if(acidente){
+
+        }else{
+          client.sendText(message.from, 'Olá,o que você deseja fazer? 1 - Ouvir Musica \n2 - Reportar acidente');
         }
-      }
-      
+       
     }
   });
 }
